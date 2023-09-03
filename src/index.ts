0ce93af759c4b69553a4a86fc70b3974c5bfa84d
Entreprenerd <alex@entreprenerd.xyz>
2023-09-03 20:11:15 +0200

    chore: extra function for arbitrary execution as demo

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import Ganache, { ServerOptions } from "ganache";
-import { parseFileAndRunGlobalLoop } from "./globalLoop";
+import theGlobalLoop, { parseFileAndRunGlobalLoop } from "./globalLoop";
+import { AdditionalSettings, DDSequence } from "./types";
 
 // Could be from ENV on test and if not being imported
 
@@ -39,6 +40,42 @@ export default async function run(
       // A single huge function which handles the whole thing
       // Receives ganache and the sequence of functions
       await parseFileAndRunGlobalLoop(ganache, folder, fileName);
+    });
+  })();
+}
+
+export async function runWithoutFile(
+  sequence: DDSequence,
+  rpcUrl?: string,
+  // Global vs Local Ganache Settings so you can fork but, for and, etc...
+  settings?: AdditionalSettings 
+) {
+  // FORK
+  const options: ServerOptions = rpcUrl
+    ? {
+        fork: {
+          url: rpcUrl,
+        },
+      }
+    : {}; // Nothing if no rpc
+
+  const server = Ganache.server(options);
+  (async () => {
+    const PORT = 0; // 0 means any available port
+    server.listen(PORT, async (err) => {
+      if (err) throw err;
+
+      console.log(`ganache listening on port ${server.address().port}...`);
+      const ganache = server.provider;
+
+      // TODO: Here you'd pass the global function expecting a ganache provider
+
+      // TODO: Expect issues with snapshotting
+      // We prob need to have a snapshot -> Execute setup if server doesn't stop
+
+      // A single huge function which handles the whole thing
+      // Receives ganache and the sequence of functions
+      await theGlobalLoop(ganache, sequence, settings ? settings : { alwaysFundCaller: true })
     });
   })();
 }
