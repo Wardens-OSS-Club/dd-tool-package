6052c311a8077b658521f8f3e5c4531947c2097f
Entreprenerd <alex@entreprenerd.xyz>
2023-09-03 19:28:54 +0200

    chore: name of file

--- src/index.ts
+++ src/index.ts
@@ -51,4 +51,4 @@ export default async function run(
 
 const RPC_URL = "https://mainnet.infura.io/v3/c7663ea6ddf54fdbaa673941846b176c";
 
-run(RPC_URL, "task", "steps");
+run(RPC_URL, "task", "steps.json");
