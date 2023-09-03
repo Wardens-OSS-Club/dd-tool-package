76b3a8d4eaa80666600658c6345b3ced5702d108
Entreprenerd <alex@entreprenerd.xyz>
2023-09-03 19:43:52 +0200

    chore: es6?

--- src/playground.ts
+++ src/playground.ts
@@ -1,8 +1,3 @@
-import run from ".";
-import executeOne from "./executor";
-import { decodeTxOutput, makeExecutableTx } from "./interpreting";
-import { theContract, theDDCall } from "./mock";
-
 const RPC_URL = "https://mainnet.infura.io/v3/c7663ea6ddf54fdbaa673941846b176c";
 
 // Call info
