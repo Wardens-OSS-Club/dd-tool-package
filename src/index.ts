10e34b83b4d8d99a65de209e852619892a4045cb
Entreprenerd <alex@entreprenerd.xyz>
2023-09-17 17:15:33 +0200

    fix: no longer using server

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,6 @@ import { AdditionalSettings, DDSequence } from "./types";
 // React will have a context with the Running Server (and a probe to ensure the server is running)
 // Then we'll have a function that does the whole thing and populates data
 // And we always revert back to state X
-
-// No rpc means new ganache without fork
 export default async function run(
   rpcUrl?: string,
   folder?: string,
@@ -16,32 +14,17 @@ export default async function run(
 ) {
   // FORK
   const options: ServerOptions = rpcUrl
-    ? {
-        fork: {
-          url: rpcUrl,
-        },
-      }
-    : {}; // Nothing if no rpc
+  ? {
+      fork: {
+        url: rpcUrl,
+      },
+    }
+  : {}; // Nothing if no rpc
 
-  const server = Ganache.server(options);
-  (async () => {
-    const PORT = 0; // 0 means any available port
-    server.listen(PORT, async (err) => {
-      if (err) throw err;
-
-      console.log(`ganache listening on port ${server.address().port}...`);
-      const ganache = server.provider;
-
-      // TODO: Here you'd pass the global function expecting a ganache provider
-
-      // TODO: Expect issues with snapshotting
-      // We prob need to have a snapshot -> Execute setup if server doesn't stop
-
-      // A single huge function which handles the whole thing
-      // Receives ganache and the sequence of functions
-      await parseFileAndRunGlobalLoop(ganache, folder, fileName);
-    });
-  })();
+  const server = Ganache.provider(options);
+  // A single huge function which handles the whole thing
+  // Receives ganache and the sequence of functions
+  await parseFileAndRunGlobalLoop(server, folder, fileName);
 }
 
 export async function runWithoutFile(
@@ -50,32 +33,19 @@ export async function runWithoutFile(
   // Global vs Local Ganache Settings so you can fork but, for and, etc...
   settings?: AdditionalSettings 
 ) {
-  // FORK
-  const options: ServerOptions = rpcUrl
+    // FORK
+    const options: ServerOptions = rpcUrl
     ? {
         fork: {
           url: rpcUrl,
         },
       }
     : {}; // Nothing if no rpc
+  
+  const server = Ganache.provider(options);
+  // A single huge function which handles the whole thing
+  // Receives ganache and the sequence of functions
+  await theGlobalLoop(server, sequence, settings ? settings : { alwaysFundCaller: true })
 
-  const server = Ganache.server(options);
-  (async () => {
-    const PORT = 0; // 0 means any available port
-    server.listen(PORT, async (err) => {
-      if (err) throw err;
-
-      console.log(`ganache listening on port ${server.address().port}...`);
-      const ganache = server.provider;
-
-      // TODO: Here you'd pass the global function expecting a ganache provider
-
-      // TODO: Expect issues with snapshotting
-      // We prob need to have a snapshot -> Execute setup if server doesn't stop
-
-      // A single huge function which handles the whole thing
-      // Receives ganache and the sequence of functions
-      await theGlobalLoop(ganache, sequence, settings ? settings : { alwaysFundCaller: true })
-    });
-  })();
+      
 }
